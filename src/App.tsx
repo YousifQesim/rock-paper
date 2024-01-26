56681f0c432cb0195a18ed3517d01f07cb9f5e00
YusifQasim <yusifqasim22@gmail.com>
2024-01-26 23:23:44 +0300

    create vite configration

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div>
+      hello world
+    </div>
+  
+    </>
+  )
+}
+
+export default App
