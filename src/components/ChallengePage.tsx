4c51da46accda30ba979b475b211b23a34f7674a
YusifQasim <yusifqasim22@gmail.com>
2024-01-27 15:46:28 +0300

    fetch result page

--- src/components/ChallengePage.tsx
+++ src/components/ChallengePage.tsx
@@ -3,7 +3,7 @@
 
 import UserScore from "./UserScore";
 
-function ChallengePage() {
+const ChallengePage: React.FC<{ playerChoice: string; computerChoice: string; winner: string }> = ({ playerChoice, computerChoice, winner }) => {
 
 
   return (
@@ -11,7 +11,7 @@ function ChallengePage() {
       <div className="flex flex-col  items-center  h-screen w-full">
           <UserScore />
         <div className="w-2/4 h-1/4 flex justify-center gap-24 relative">
-        
+        <h1>{winner}</h1>
         </div>
       </div>
     </div>
--- src/components/UserPage.tsx
+++ src/components/UserPage.tsx
@@ -8,7 +8,11 @@ import { UseDispatch,useDispatch,useSelector } from "react-redux";
 import { RootState } from "../redux/Store";
 import UserScore from "./UserScore";
 import { setScore,setComputerChoice,setPlayerChoice,resetGame} from "../redux/GameSlice";
+import { useState } from "react";
+import ChallengePage from "./ChallengePage";
+
 const optoins=['rock','paper','scissors']
+
 
 
 const determineWinner = (playerChoice: string, computerChoice: string): string => {
@@ -26,51 +30,51 @@ const determineWinner = (playerChoice: string, computerChoice: string): string =
 function UserPage() {
   const dispatch=useDispatch();
   const { score, playerChoice, computerChoice } = useSelector((state: RootState) => state.game);
-  
+  const [showResultPage,setShowResultPage]=useState(false)
   const handleChoice=(choice:string)=>{
     const computerChoice=optoins[Math.floor(Math.random()*3)]
     dispatch(setComputerChoice(computerChoice));
     const winner = determineWinner(choice, computerChoice);
 
-    if (winner === 'player') {
-      dispatch(setScore(score + 1));
-    } else if (winner === 'computer') {
-      dispatch(setScore(score - 1));
-    }
+    dispatch(setScore(score + (winner === 'player' ? 1 : winner === 'computer' ? -1 : 0)));
+
+    // Show the result page after making a choice
+    setShowResultPage(true);
     dispatch(setPlayerChoice(choice));
   
   }
   return (
     <div className="h-screen ">
+        {showResultPage ? (
+        <ChallengePage playerChoice={playerChoice} computerChoice={computerChoice} winner={determineWinner(playerChoice, computerChoice)} />
+      ) :(
       
       <div className="flex flex-col  items-center  h-screen w-full">
-          <UserScore />
+          <UserScore/>
         <div className="w-2/4 h-1/4 flex justify-center gap-24 relative">
           {/* <div className="line1"></div> */}
 
-          <div className="bg-white rounded-full h-20 w-20 flex justify-center items-center outline outline-8 outline-indigo-600 z-50" onClick={() => handleChoice('paper')}>
+          <div className="bg-white cursor-pointer rounded-full h-20 w-20 flex justify-center items-center outline outline-8 outline-indigo-600 z-50 " onClick={() => handleChoice('paper')}>
             <div className="flex items-center">
               <img src={paper} alt="" />
             </div>
           </div>
           {/* <div className="line2"></div> */}
-          <div className="bg-white rounded-full h-20 w-20 flex justify-center items-center outline outline-8 outline-yellow-500 z-50" onClick={() => handleChoice('scissors')}>
+          <div className="bg-white cursor-pointer rounded-full h-20 w-20 flex justify-center items-center outline outline-8 outline-yellow-500 z-50" onClick={() => handleChoice('scissors')}>
             <div className="flex items-center">
               <img src={scissors} alt="" />
             </div>
           </div>
           {/* <div className="line3"></div> */}
-          <div className="bg-white absolute -bottom-5 rounded-full h-20 w-20 flex justify-center items-center outline outline-8 outline-red-600 z-50" onClick={() => handleChoice('rock')}>
+          <div className="bg-white cursor-pointer absolute -bottom-5 rounded-full h-20 w-20 flex justify-center items-center outline outline-8 outline-red-600 z-50" onClick={() => handleChoice('rock')}>
             <div className="flex items-center ">
               <img src={rock} alt="" />
             </div>
           </div>
         </div>
-      </div>
-      <p>Player: {playerChoice}</p>
-      <p>Computer: {computerChoice}</p>
-      <p>Result: {determineWinner(playerChoice, computerChoice)}</p>
-      
+      </div>)
+    
+}
     </div>
   );
 }
