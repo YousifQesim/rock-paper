d9e43c2bb6e66caa5c63b561e799918cd6edc28e
YusifQasim <yusifqasim22@gmail.com>
2024-01-28 22:36:09 +0300

    update ChallengePage.tsx

--- src/components/ChallengePage.tsx
+++ src/components/ChallengePage.tsx
@@ -1,8 +1,6 @@
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-
-import { RootState } from "@/redux/Store";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "@/redux/Store";
 import Paper from "./Paper";
 import Rock from "./Rock";
 import Scissors from "./Scissors";
@@ -11,11 +9,15 @@ import Spock from "./Spock";
 import { determineWinner, resetGame, setResultPage } from "../redux/GameSlice";
 import PopupButton from "./PopupButton";
 import Popup from "./Popup";
-import { useEffect, useState } from "react";
 import Spinner from "./Spinner";
 
+// Import sound files
+import winSound from "../assets/sounds/success-fanfare-trumpets-6185.mp3";
+import loseSound from "../assets/sounds/violin-lose-5-185126.mp3";
+import drawSound from "../assets/sounds/negative_beeps-6008.mp3";
+
 const ChallengePage = () => {
-  const { playerChoice, computerChoice,score } = useSelector(
+  const { playerChoice, computerChoice, score } = useSelector(
     (state: RootState) => state.game
   );
   const winner = useSelector(determineWinner);
@@ -23,13 +25,35 @@ const ChallengePage = () => {
 
   const [isPopupVisible, setPopupVisible] = useState(false);
   const [isLoading, setLoading] = useState(true);
+
+  // Create audio elements
+  const winAudio = new Audio(winSound);
+  const loseAudio = new Audio(loseSound);
+  const drawAudio = new Audio(drawSound);
+
+  // Function to play the appropriate sound based on the game outcome
+  const playSound = () => {
+    if (winner === "player") {
+      winAudio.play();
+    } else if (winner === "computer") {
+      loseAudio.play();
+    } else {
+      drawAudio.play();
+    }
+  };
+
+  // Function to handle game reset
   const handleReset = () => {
     dispatch(resetGame());
     dispatch(setResultPage(false));
   };
+
+  // Function to toggle the popup visibility
   const togglePopup = () => {
     setPopupVisible(!isPopupVisible);
   };
+
+  // Function to render player's choice component
   const renderPlayerChoiceComponent = () => {
     switch (playerChoice) {
       case "rock":
@@ -64,12 +88,13 @@ const ChallengePage = () => {
           </div>
         );
       default:
-        return null; // Handle other cases or unexpected values
+        return null;
     }
   };
 
+  // Function to render computer's choice component
   const renderComputerChoiceComponent = () => {
-      switch (computerChoice) {
+    switch (computerChoice) {
       case "rock":
         return (
           <div className="bg-white cursor-pointer rounded-full h-28 w-28 flex justify-center items-center border-12 border-rock  z-50">
@@ -101,46 +126,44 @@ const ChallengePage = () => {
             <Spock />
           </div>
         );
-
       default:
-        return null; // Handle other cases or unexpected values
+        return null;
     }
   };
+
   useEffect(() => {
     // Simulate loading for computer's choice (example with setTimeout)
     const timeoutId = setTimeout(() => {
       setLoading(false);
+      playSound(); // Play the sound when the computer makes a choice
     }, 2000);
 
     // Cleanup the timeout on component unmount or whenever it's appropriate
     return () => clearTimeout(timeoutId);
   }, [computerChoice]);
+
   return (
     <div className="h-screen ">
       <div className="flex flex-col  items-center w-full ">
-    
-    
-      <div className="border-white border-solid border-2 rounded-lg text-white md:w-1/4 w-3/4 h-24 flex justify-between items-center mx-auto my-20">
-      <div className="ml-4 font-rockFamily space-y-0 leading-4 uppercase font-700 ">
-        <h1>Rock</h1>
-        <h1>Paper</h1>
-        <h1>Scissors</h1>
-        <h1>Lizard</h1>
-        <h1>spock</h1>
-      </div>
-      <div className="mr-4 bg-white text-black p-2 w-24 rounded-xl text-center">
-        <h1 className=" font-rockFamily font-700">score</h1>
-        {isLoading ? (
-            <div className="">
-          
+        <div className="border-white border-solid border-2 rounded-lg text-white md:w-1/4 w-3/4 h-24 flex justify-between items-center mx-auto my-20">
+          <div className="ml-4 font-rockFamily space-y-0 leading-4 uppercase font-700">
+            <h1>Rock</h1>
+            <h1>Paper</h1>
+            <h1>Scissors</h1>
+            <h1>Lizard</h1>
+            <h1>Spock</h1>
+          </div>
+          <div className="mr-4 bg-white text-black p-2 w-24 rounded-xl text-center">
+            <h1 className="font-rockFamily font-700">Score</h1>
+            {isLoading ? (
+              <div>
                 <h1 className="animate-pulse font-rockFamily font-700 text-4xl text-gray-600">...</h1>
-            </div>
-          ) : (
-        <h1 className="font-rockFamily font-700 text-4xl text-gray-600 ">{score}</h1>)}
-      </div>
-    </div>
-
-
+              </div>
+            ) : (
+              <h1 className="font-rockFamily font-700 text-4xl text-gray-600 ">{score}</h1>
+            )}
+          </div>
+        </div>
 
         <div className="md:w-2/4 w-full h-1/4 flex justify-center items-center gap-12 flex-wrap">
           <div className="flex flex-col justify-center items-center">
@@ -154,64 +177,59 @@ const ChallengePage = () => {
 
           {isLoading ? (
             <div className="order-last lg:order-none md:mx-12 lg:relative lg:left-6">
-          
-        <Spinner/>
+              <Spinner />
             </div>
           ) : (
-
-          <div className="order-last lg:order-none md:mx-12 ">
-            {winner ? (
-              winner === "player" ? (
-                <div className="flex flex-col justify-center items-center">
-                  <h1 className="text-white font-rockFamily font-700 text-2xl uppercase">
-                    You win
-                  </h1>
-                  <button
-                    className="text-green-600 bg-white p-2 rounded-xl w-36 mt-2"
-                    onClick={handleReset}
-                  >
-                    Play Again
-                  </button>
-                </div>
-              ) : winner === "computer" ? (
-                <div className="flex flex-col justify-center items-center">
-                  <h1 className="text-white font-rockFamily font-700 text-2xl uppercase">
-                    Computer wins
-                  </h1>
-                  <button
-                    className="text-red-600 bg-white p-2 rounded-xl w-36 mt-2"
-                    onClick={handleReset}
-                  >
-                    Play Again
-                  </button>
-                </div>
-              ) : (
-                <div className="flex flex-col justify-center items-center">
-                  <h1 className="text-white font-rockFamily font-700 text-2xl uppercase">
-                    Draw
-                  </h1>
-                  <button
-                    className="text-orange-400 bg-white p-2 rounded-xl w-36 mt-2"
-                    onClick={handleReset}
-                  >
-                    Play Again
-                  </button>
-                </div>
-              )
-            ) : null}
-          </div>
+            <div className="order-last lg:order-none md:mx-12">
+              {winner ? (
+                winner === "player" ? (
+                  <div className="flex flex-col justify-center items-center">
+                    <h1 className="text-white font-rockFamily font-700 text-2xl uppercase">
+                      You win
+                    </h1>
+                    <button
+                      className="text-green-600 bg-white p-2 rounded-xl w-36 mt-2"
+                      onClick={handleReset}
+                    >
+                      Play Again
+                    </button>
+                  </div>
+                ) : winner === "computer" ? (
+                  <div className="flex flex-col justify-center items-center">
+                    <h1 className="text-white font-rockFamily font-700 text-2xl uppercase">
+                      Computer wins
+                    </h1>
+                    <button
+                      className="text-red-600 bg-white p-2 rounded-xl w-36 mt-2"
+                      onClick={handleReset}
+                    >
+                      Play Again
+                    </button>
+                  </div>
+                ) : (
+                  <div className="flex flex-col justify-center items-center">
+                    <h1 className="text-white font-rockFamily font-700 text-2xl uppercase">
+                      Draw
+                    </h1>
+                    <button
+                      className="text-orange-400 bg-white p-2 rounded-xl w-36 mt-2"
+                      onClick={handleReset}
+                    >
+                      Play Again
+                    </button>
+                  </div>
+                )
+              ) : null}
+            </div>
           )}
           {isLoading ? (
             <div className="flex flex-col justify-center items-center">
-          
               <div className="my-8">
                 <h1 className="text-white font-rockFamily font-600 text-xl">
                   Waiting House To pick...
                 </h1>
               </div>
-      
-                <div className="  bg-white cursor-pointer rounded-full h-28 w-28 flex justify-center items-center border-12 border-gray-500 animate-pulse"></div>
-          
+              <div className="  bg-white cursor-pointer rounded-full h-28 w-28 flex justify-center items-center border-12 border-gray-500 animate-pulse"></div>
             </div>
           ) : (
             <div className="flex flex-col justify-center items-center">
